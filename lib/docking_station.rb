a59d01eb6aab1e1908278dd760b6e1ff0e44958f
robertamangiapane <roberta.mangiapane@gmail.com>
2019-09-20 15:46:59 +0100

    Step 17 Adding custom capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,30 +2,32 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :docked_bikes
+  attr_reader :bikes
+  attr_reader :capacity
   DEFAULT_CAPACITY = 20
 
-  def initialize
-    @docked_bikes = []
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
   end
 
   def release_bike
     fail 'No bikes available' if empty?
-    @docked_bikes.pop
+    bikes.pop
   end
 
   def dock_bike(bike)
    fail 'Docking station full' if full?
-   @docked_bikes << bike
+   bikes << bike
   end
 
   private
 
   def full?
-    return true if @docked_bikes.count >= DEFAULT_CAPACITY
+    return true if bikes.count >= capacity
   end
 
   def empty?
-    return true if @docked_bikes.count == 0
+    return true if bikes.count == 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,43 +1,56 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
 
-    # Use a '#' before a method name to imply that it is an instance method.
-     # Until now, we have been describing behaviour in a general sense. However,
-     # We are now describing behaviour specific to a particular method.
-     # By nesting this in another describe block, we are able to DRY out our
-     # descriptions.
-    describe '#release_bike' do
-      it 'releases a bike' do
-        bike = Bike.new
+  let(:bike) { Bike.new }
+
+=begin
+  it 'station has a default capacity' do
+    expect(subject.capacity).to eq described_class::DEFAULT_CAPACITY
+  end
+=end
+  describe 'initialization' do
+    it "defaults capacity" do
+      described_class::DEFAULT_CAPACITY.times do
         subject.dock_bike(bike)
-        #release the bike we docked
-        expect(subject.release_bike).to eq bike
-      end
-
-      it 'raises an error when no bikes available' do
-        expect { subject.release_bike }.to raise_error 'No bikes available'
       end
     end
+  end
 
-    describe '#dock_bike' do
-      it { is_expected.to respond_to(:dock_bike).with(1).argument }
+  it 'station has a custom capacity' do
+    docking_station = DockingStation.new(30)
+    30.times{docking_station.dock_bike(bike)}
+    expect { docking_station.dock_bike(bike) }.to raise_error 'Docking station full'
+  end
 
-      it "docks a bike" do
-        bike = Bike.new
-        expect(subject.dock_bike(bike)).to eq subject.docked_bikes
-      end
+  it { is_expected.to respond_to :release_bike }
 
-      it "returns docked bike" do
-        bike = Bike.new
-        # Again, we need to return the bike we just docked
-        expect(subject.dock_bike(bike)).to eq subject.docked_bikes
-      end
-
-      it 'raises an error when bike capacity hits 20' do
-        DockingStation::DEFAULT_CAPACITY.times{subject.dock_bike(Bike.new)}
-        expect { subject.dock_bike(Bike.new) }.to raise_error 'Docking station full'
-      end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
     end
+
+    it 'raises an error when no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock_bike' do
+    it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+    it "docks a bike" do
+      expect(subject.dock_bike(bike)).to eq subject.bikes
+    end
+
+    it "returns docked bike" do
+      expect(subject.dock_bike(bike)).to eq subject.bikes
+    end
+=begin
+    it 'raises an error when station is full' do
+      subject.capacity.times{subject.dock_bike(bike)}
+      expect { subject.dock_bike(bike) }.to raise_error 'Docking station full'
+    end
+=end
+  end
 end
